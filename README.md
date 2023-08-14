2224f6a494bce36c9099483da58131c3309baadd
amSebas <sebaslgr@gmail.com>
2023-08-14 11:37:11 -0500

    home

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Librerias instaladas: 
     -reacr-router-dom: instalo react router dom usando la consola git bash con el comando "npm install react-router-dom"
+    ![Alt text](./src/images/Documentacion1.PNG)
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,29 @@
 import '../styles/home.css'
+import CV1 from '../images/CV1.png'
 
 export default function Home () {
     return(
-        <header className="header">
+        <div className='home'>
             <h1>Home</h1>
-        </header>
+            <div className='box-1'>
+                <div className='text-box'>
+                    <h2>Hola!, Soy sebas</h2>
+                    <h3>Desarrollador web front-end</h3>
+                    <h3>Como desarrollador front-end me especializo en crear sitios web dinamicos y responsivos haciendo uso de herramientas como HTML5, CSS3, JavaScript,Figma, librerias como react.js,
+                        node.js y conocimiento de metodologias agiles.
+                    </h3>
+                    <a className='btn' href='./AboutMe'>Mas acerca de mi</a>
+                    <a className='btn' href='/'>Descargar cv</a>
+                    <div className='social-media'>
+                        <a href='/'><i className="fa-brands fa-linkedin"></i></a>
+                        <a href='/'><i className="fa-brands fa-github"></i></a>
+                        <a href='/'><i className="fa-brands fa-whatsapp"></i></a>
+                    </div>
+                </div>
+                <div className='photo-presentation'>
+                    <img src={CV1} alt="CV" />
+                </div>
+            </div>
+        </div>  
     );
 }
